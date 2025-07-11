8843403845c3fc721e5489eaad9292779189adab
ari <communeofstars@proton.me>
2025-07-11 10:51:20 +0000

    feat (nvim): autocmds to toggle relativenumber based on context

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,12 +1,49 @@
 local autocmd = vim.api.nvim_create_autocmd
+vim.api.nvim_create_augroup("augroup", { clear = true })
 
 -- Highlight yank
-vim.api.nvim_create_augroup("Extras", { clear = true })
 autocmd("TextYankPost", {
-	group = "Extras",
+	group = "augroup",
 	pattern = "*",
 	callback = function()
 		vim.highlight.on_yank({ timeout = 100 })
 	end,
 })
 
+-- Restore cursor position on file open
+autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		local line = vim.fn.line("'\"")
+		if line > 1 and line <= vim.fn.line("$") then
+			vim.cmd("normal! g'\"")
+		end
+	end,
+})
+
+--[[
+Disable relativenumber outside normal mode
+]]
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+	pattern = "*",
+	group = "augroup",
+	callback = function()
+		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+			vim.opt.relativenumber = true
+		end
+	end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+	pattern = "*",
+	group = "augroup",
+	callback = function()
+		if vim.o.nu then
+			vim.opt.relativenumber = false
+			if not vim.tbl_contains({ "@", "-" }, vim.v.event.cmdtype) then
+				vim.cmd("redraw")
+			end
+		end
+	end,
+})
+--
