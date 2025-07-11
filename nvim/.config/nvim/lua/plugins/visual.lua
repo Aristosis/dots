8e74ec908e3589347f72fab3364083fde2325670
ari <communeofstars@proton.me>
2025-07-11 10:49:54 +0000

    feat (nvim): add nvim colorizer

--- nvim/.config/nvim/lua/plugins/visual.lua
+++ nvim/.config/nvim/lua/plugins/visual.lua
@@ -9,12 +9,34 @@ return {
 		},
 	},
 
+	-- colorizer
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				names = false, -- "Name" codes like Blue
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
+
+	-- fidget
+	{
+		"j-hui/fidget.nvim",
+		tag = "v1.6.1",
+		opts = {},
+	},
+
 	-- indent-blankline
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
-		---@module "ibl"
-		---@type ibl.config
 		opts = {
 			scope = {
 				show_start = false,
@@ -23,7 +45,7 @@ return {
 		},
 	},
 
-	-- fidget
+	-- gitsigns
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
